afa8eacc6741deb3878b928fc482555681dfe9f4
chola10 <cholakabaso10@gmail.com>
2023-01-13 23:48:39 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-
-    new_string = my_string.translate({ord(i): None for i in 'cC'})
-
-    return new_string
-
+    new_str = ""
+    for i in range(len(my_string)):
+        if my_string[i] == 'C' or my_string[i] == 'c':
+            pass
+        else:
+            new_str += my_string[i]
+    return new_str
