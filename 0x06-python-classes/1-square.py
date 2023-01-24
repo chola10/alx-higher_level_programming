16b2c7b38df00b198f30f166c81c21f767206cbd
chola10 <cholakabaso10@gmail.com>
2023-01-24 17:31:30 +0000

    task 8

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"0-square.py define"
+
+
+class Square:
+    """Class Samu
+    """
+
+    def __init__(self, size):
+        """Def
+        """
+        self.__size = size
+        
