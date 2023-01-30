ff104c3bd6c976007cce93df46e6bd9e15d95a82
chola10 <cholakabaso10@gmail.com>
2023-01-30 06:55:31 -0800

    task 10

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,19 +1,5 @@
 #!/usr/bin/python3
-"""Solves the N-queens puzzle.
-Determines all possible solutions to placing N
-N non-attacking queens on an NxN chessboard.
-Example:
-    $ ./101-nqueens.py N
-N must be an integer greater than or equal to 4.
-Attributes:
-    board (list): A list of lists representing the chessboard.
-    solutions (list): A list of lists containing solutions.
-Solutions are represented in the format [[r, c], [r, c], [r, c], [r, c]]
-where `r` and `c` represent the row and column, respectively, where a
-queen must be placed on the chessboard.
-"""
 import sys
-
 
 def init_board(n):
     """Initialize an `n`x`n` sized chessboard with 0's."""
@@ -31,7 +17,7 @@ def board_deepcopy(board):
 
 
 def get_solution(board):
-    """Return the list of lists representation of a solved chessboard."""
+  
     solution = []
     for r in range(len(board)):
         for c in range(len(board)):
@@ -42,14 +28,7 @@ def get_solution(board):
 
 
 def xout(board, row, col):
-    """X out spots on a chessboard.
-    All spots where non-attacking queens can no
-    longer be played are X-ed out.
-    Args:
-        board (list): The current working chessboard.
-        row (int): The row where a queen was last played.
-        col (int): The column where a queen was last played.
-    """
+
     # X out all forward spots
     for c in range(col + 1, len(board)):
         board[row][c] = "x"
